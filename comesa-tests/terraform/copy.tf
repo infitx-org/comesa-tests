5605d9f2e87523070c188ba4be3656508e19354a
Kalin Krustev <kalin.krustev@gmail.com>
2024-12-23 17:06:37 +0000

    fix: deploy

--- comesa-tests/terraform/copy.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "ttk-files" {
-    for_each = fileset(dirname(path.module), "{collections,environments}/**")
-    content  = file("${dirname(path.module)}/${each.value}")
-    filename = "${var.outputDir}/comesa-tests/environments/${each.value}"
-}
-
-variable "outputDir" {
-  type = string
-}
--- comesa-tests/terraform/terragrunt.hcl
+++
@@ -1,7 +0,0 @@
-inputs = {
-  outputDir = get_env("GITOPS_BUILD_OUTPUT_DIR")
-}
-
-terraform {
-  copy_terraform_lock_file = false
-}
